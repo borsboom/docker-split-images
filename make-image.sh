e6c35a9e036abfa3676c99f0a3b4f9624eeb080b
Emanuel Borsboom <emanuel@borsboom.io>
2015-12-13 07:05:53 -0800

    Split-image approach

--- make-image.sh
+++ make-image.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 set -xe
-docker build -t haskell-hello build/
+docker build -t build_haskell-hello build/
+docker run --rm \
+    --volume="$PWD/build/src:/src" \
+    --volume="$PWD/run/artifacts:/artifacts" \
+    build_haskell-hello
+docker build -t haskell-hello run/
